7d384079c24bd6f44101eea08b9e73a4b70021ba
varun pandian <30491782+varunpandian@users.noreply.github.com>
2017-08-15 00:01:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/',function(req,res) {
+app.get('/articleone',function(req,res) {
      res.sendFile(path.join(__dirname, 'ui', 'articleone.html'));
 });
 
